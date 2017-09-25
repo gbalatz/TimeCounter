c4802a9b8b6db42ffd3b8b14a68feed5d3b78758
Georgios Balatzis <balatzgs@studi.informatik.uni-stuttgart.de>
2017-09-25 16:02:23 +0200

    Updated times

--- pause.py
+++ pause.py
@@ -13,8 +13,8 @@ now = datetime.datetime.now()
 
 
 ### DateTime Variables ###
-first = start + datetime.timedelta(minutes=360)
-second = start + datetime.timedelta(minutes=510)
+first = start + datetime.timedelta(minutes=359)
+second = start + datetime.timedelta(minutes=539)
 last = start + datetime.timedelta(minutes=645)
 elapsed = now - start
 
@@ -27,7 +27,7 @@ print ("Time elapsed since work started: " + str(elapsed.seconds/3600) +":"+ str
 ### Output on breaks ###
 if(elapsed.seconds) <= 6*3600 :
     print "You have to go on a break in: " + str((6*3600 - elapsed.seconds)/60) + " minutes\n"
-elif(elapsed) > (6*3600) and (elapsed) <=(9*3600):
+elif(elapsed.seconds) > (6*3600) and (elapsed.seconds) <=(9*3600):
     print "You already had a 30 minutes Break \n"
 else:
     print "You are working on the limit and getting another 15 Minutes of break \n"
